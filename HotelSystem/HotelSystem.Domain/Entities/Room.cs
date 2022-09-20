3e8f630c69fc923ffd6841206f751ddba930629f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-09-20 19:46:48 +0200

    Update entity of Room

--- HotelSystem/HotelSystem.Domain/Entities/Room.cs
+++ HotelSystem/HotelSystem.Domain/Entities/Room.cs
@@ -14,5 +14,9 @@ namespace HotelSystem.Domain.Entities
         public int Price { get; set; }
         public bool Avability { get; set; }
         public string Description { get; set; }
+        public int AmenitiesId { get; set; }
+        public Amenities Amenities { get; set; }
+        public User User { get; set; }
+        public Reservation Reservation { get; set; }
     }
 }
