d9dcbde0464d0701818c92696c59abfcb1376f55
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-09-20 19:30:03 +0200

    Added entity of Amenities

---
+++ HotelSystem/HotelSystem.Domain/Entities/Amenities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Domain.Entities
+{
+    public class Amenities
+    {
+        public int Id { get; set; }
+        public bool EntireApartment { get; set; }
+        public bool PrivateBathroom { get; set; }
+        public bool PrivateKitchenette { get; set; }
+        public bool CityView { get; set; }
+        public bool AirConditioning { get; set; }
+        public bool Patio { get; set; }
+        public bool Dishwasher { get; set; }
+        public bool Tv { get; set; }
+        public bool CoffeeMachine { get; set; }
+        public bool WiFi { get; set; }
+    }
+}
