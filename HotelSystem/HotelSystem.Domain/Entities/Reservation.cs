54115bc385670a16f6cfe382f624350facb6392a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-09-20 19:17:41 +0200

    Added entity of Reservation

---
+++ HotelSystem/HotelSystem.Domain/Entities/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Domain.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Mail { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public bool Payment { get; set; }
+    }
+}
