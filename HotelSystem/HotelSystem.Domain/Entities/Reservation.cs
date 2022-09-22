f07a68b458ec62f9bb81c5ec5e18cb914a42bb43
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-09-22 22:45:59 +0200

    Uptade model of Reservation with AuditableEntity

--- HotelSystem/HotelSystem.Domain/Entities/Reservation.cs
+++ HotelSystem/HotelSystem.Domain/Entities/Reservation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelSystem.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,8 @@ using System.Threading.Tasks;
 
 namespace HotelSystem.Domain.Entities
 {
-    public class Reservation
+    public class Reservation : AuditableEntity
     {
-        public int Id { get; set; }
         public int Price { get; set; }
         public string Mail { get; set; }
         public DateTime CheckIn { get; set; }
