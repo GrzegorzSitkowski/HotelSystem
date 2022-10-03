656dbbba6250f0b343f44f60725dd2a949503c84
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-03 18:47:20 +0200

    Implemented HotelDbContextFactory

---
+++ HotelSystem/HotelSystem.Persistance/HotelDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Persistance
+{
+    public class HotelDbContextFactory : DesignTimeDbContextFactoryBase<HotelDbContext>
+    {
+        protected override HotelDbContext CreateNewInstance(DbContextOptions<HotelDbContext> options)
+        {
+            return new HotelDbContext(options);
+        }
+    }
+}
