adf5e983f6f51b1fe941378eecf141d577060343
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-03 20:46:39 +0200

    Added record of Amenities in SeedData

---
+++ HotelSystem/HotelSystem.Persistance/Seed.cs
@@ -0,0 +1,33 @@
+﻿using HotelSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Persistance
+{
+    public static class Seed
+    {
+        public static void SeedData(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Amenities>().HasData(
+                new Amenities()
+                {
+                    Id = 1,
+                    EntireApartment = true,
+                    PrivateBathroom = true,
+                    PrivateKitchenette = true,
+                    CityView = false,
+                    AirConditioning = true,
+                    Patio = false,
+                    Dishwasher = false,
+                    Tv = true,
+                    CoffeeMachine = false,
+                    WiFi = true
+                }
+                );
+        }
+    }
+}
