51cb5704943ae767d4735f77ff17d90a4bf5af6a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-05 19:41:49 +0200

    Configuration DepedencyInjection

---
+++ HotelSystem/HotelSystem.Infrastructure/DepedencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using HotelSystem.Application.Interfaces;
+using HotelSystem.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Infrastructure
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IDateTime, DateTimeService>();
+            return services;
+        }
+    }
+}
