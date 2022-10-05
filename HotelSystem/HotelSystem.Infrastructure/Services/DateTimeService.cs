5f1ff754600b9cf32cedeb5e4efa7c076af8eee9
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-05 19:34:29 +0200

    Created IDateTime

---
+++ HotelSystem/HotelSystem.Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Infrastructure.Services
+{
+    public class DateTimeService : IDateTime
+    {
+    }
+}
