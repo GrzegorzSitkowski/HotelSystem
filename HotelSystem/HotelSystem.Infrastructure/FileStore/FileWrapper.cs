7d0fc89f4ff37c127cfe8dd0bbc5920a9f9977ce
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-06 20:45:41 +0200

    Implemented FileWrapper

---
+++ HotelSystem/HotelSystem.Infrastructure/FileStore/FileWrapper.cs
@@ -0,0 +1,18 @@
+﻿using HotelSystem.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Infrastructure.FileStore
+{
+    public class FileWrapper : IFileWrapper
+    {
+        public void WriteAllBytes(string outputFile, byte[] content)
+        {
+            File.WriteAllBytes(outputFile, content);
+        }
+    }
+}
