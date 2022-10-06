b3670029c56819fdf5dc9bf5a5217a65b50107a0
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-06 20:42:39 +0200

    Added IFileStore

---
+++ HotelSystem/HotelSystem.Application/Interfaces/IFileStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Interfaces
+{
+    public interface  IFileStore
+    {
+        string SaveWriteFile(byte[] content, string sourceFileName, string path);
+    }
+}
