ea4f3936681dfd98b0a3762c2618e6ee81dbe0e6
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-08 21:55:17 +0200

    Implemented IHotelDbContext

---
+++ HotelSystem/HotelSystem.Application/Interfaces/IHotelDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HotelSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Interfaces
+{
+    public interface IHotelDbContext
+    {
+        DbSet<Amenities> Amenities { get; set; }
+        DbSet<Reservation> Reservations { get; set; }
+        DbSet<Room> Rooms { get; set; }
+        DbSet<User> Users { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
