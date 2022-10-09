4f951bf12c955af2481c7442e77c6cac2f955b3b
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-09 19:30:29 +0200

    Filled GetRoomDetailQueryHandler

--- HotelSystem/HotelSystem.Application/Rooms/Queries/GetRoomDetail/GetRoomDetailQueryHandler.cs
+++ HotelSystem/HotelSystem.Application/Rooms/Queries/GetRoomDetail/GetRoomDetailQueryHandler.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using HotelSystem.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HotelSystem.Application.Rooms.Queries.GetRoomDetail
 {
-    public class GetRoomDetailQueryHandler
+    public class GetRoomDetailQueryHandler : IRequestHandler<GetRoomDetailQuery, RoomDetailVm>
     {
+        private readonly IHotelDbContext _context;
+        public GetRoomDetailQueryHandler(IHotelDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<RoomDetailVm> Handle(GetRoomDetailQuery request, CancellationToken cancellationToken)
+        {
+            var room = await _context.Rooms.Where(p => p.Id == request.Id).FirstOrDefaultAsync(cancellationToken);
+
+            var roomVm = new RoomDetailVm()
+            {
+                Name = room.Name,
+                Capacity = room.Capacity,
+                Price = room.Price,
+                Avability = room.Avability,
+                Description = room.Description
+            };
+
+            return roomVm;
+        }
     }
 }
