95a54f84ed5bcc5fc478ef82b1ee93c37297fbcd
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-09 20:28:15 +0200

    Added DeleteRoomCommandHandler

---
+++ HotelSystem/HotelSystem.Application/Rooms/Commands/DeleteRoom/DeleteRoomCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using HotelSystem.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Rooms.Commands.DeleteRoom
+{
+    public class DeleteRoomCommandHandler : IRequestHandler<DeleteRoomCommand>
+    {
+        private readonly IHotelDbContext _context;
+        public DeleteRoomCommandHandler(IHotelDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(DeleteRoomCommand request, CancellationToken cancellationToken)
+        {
+           var room = await _context.Rooms.Where(p => p.Id == request.Id).FirstOrDefaultAsync(cancellationToken);
+
+            _context.Rooms.Remove(room);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
