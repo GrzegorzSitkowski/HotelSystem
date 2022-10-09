428b1c938dffe8c8fbe0fab561c579cb01ae6044
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-09 20:18:40 +0200

    Added UpdateRoomCommand

---
+++ HotelSystem/HotelSystem.Application/Rooms/Commands/UpdateRoom/UpdateRoomCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Rooms.Commands.UpdateRoom
+{
+    public class UpdateRoomCommand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Price { get; set; }
+        public bool Avability { get; set; }
+        public string Description { get; set; }
+    }
+}
