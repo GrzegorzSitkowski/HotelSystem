70c361cd13aa50803d2cb2a5550c8565eed90170
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-09 19:18:02 +0200

    Filled RoomDetailVm

--- HotelSystem/HotelSystem.Application/Rooms/Queries/GetRoomDetail/RoomDetailVm.cs
+++ HotelSystem/HotelSystem.Application/Rooms/Queries/GetRoomDetail/RoomDetailVm.cs
@@ -8,5 +8,11 @@ namespace HotelSystem.Application.Rooms.Queries.GetRoomDetail
 {
     public class RoomDetailVm
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Price { get; set; }
+        public bool Avability { get; set; }
+        public string Description { get; set; }
     }
 }
