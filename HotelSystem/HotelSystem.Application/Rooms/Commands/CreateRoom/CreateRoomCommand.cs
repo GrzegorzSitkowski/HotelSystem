5c26c3fa51dcfdb26abdd44ef2c6a092e04e4584
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-09 19:44:36 +0200

    Added CreateRoomCommand

---
+++ HotelSystem/HotelSystem.Application/Rooms/Commands/CreateRoom/CreateRoomCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Rooms.Commands.CreateRoom
+{
+    public class CreateRoomCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Price { get; set; }
+        public bool Avability { get; set; }
+        public string Description { get; set; }
+    }
+}
