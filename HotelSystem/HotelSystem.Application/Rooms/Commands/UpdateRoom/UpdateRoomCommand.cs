394c7ce95931e12b93298495decc1616c23559ec
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-09 20:24:03 +0200

    Added UpdateRoomCommandHandler

--- HotelSystem/HotelSystem.Application/Rooms/Commands/UpdateRoom/UpdateRoomCommand.cs
+++ HotelSystem/HotelSystem.Application/Rooms/Commands/UpdateRoom/UpdateRoomCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HotelSystem.Application.Rooms.Commands.UpdateRoom
 {
-    public class UpdateRoomCommand
+    public class UpdateRoomCommand : IRequest
     {
         public int Id { get; set; }
         public string Name { get; set; }
