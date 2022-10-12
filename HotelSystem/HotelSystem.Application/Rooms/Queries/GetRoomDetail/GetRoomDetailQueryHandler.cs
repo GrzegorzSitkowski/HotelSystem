3e525816e9e2d524c4685de9a63bddba53f52e16
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-12 22:49:40 +0200

    Mapping in GetRoomDetail

--- HotelSystem/HotelSystem.Application/Rooms/Queries/GetRoomDetail/GetRoomDetailQueryHandler.cs
+++ HotelSystem/HotelSystem.Application/Rooms/Queries/GetRoomDetail/GetRoomDetailQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using HotelSystem.Application.Interfaces;
+﻿using AutoMapper;
+using HotelSystem.Application.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -13,22 +14,17 @@ namespace HotelSystem.Application.Rooms.Queries.GetRoomDetail
     public class GetRoomDetailQueryHandler : IRequestHandler<GetRoomDetailQuery, RoomDetailVm>
     {
         private readonly IHotelDbContext _context;
-        public GetRoomDetailQueryHandler(IHotelDbContext context)
+        private readonly IMapper _mapper;
+        public GetRoomDetailQueryHandler(IHotelDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
         public async Task<RoomDetailVm> Handle(GetRoomDetailQuery request, CancellationToken cancellationToken)
         {
             var room = await _context.Rooms.Where(p => p.Id == request.Id).FirstOrDefaultAsync(cancellationToken);
 
-            var roomVm = new RoomDetailVm()
-            {
-                Name = room.Name,
-                Capacity = room.Capacity,
-                Price = room.Price,
-                Avability = room.Avability,
-                Description = room.Description
-            };
+            var roomVm = _mapper.Map<RoomDetailVm>(room);
 
             return roomVm;
         }
--- HotelSystem/HotelSystem.Application/Rooms/Queries/GetRoomDetail/RoomDetailVm.cs
+++ HotelSystem/HotelSystem.Application/Rooms/Queries/GetRoomDetail/RoomDetailVm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using HotelSystem.Application.Mappings;
+using HotelSystem.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace HotelSystem.Application.Rooms.Queries.GetRoomDetail
 {
-    public class RoomDetailVm
+    public class RoomDetailVm : IMapFrom<Room>
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -14,5 +17,10 @@ namespace HotelSystem.Application.Rooms.Queries.GetRoomDetail
         public int Price { get; set; }
         public bool Avability { get; set; }
         public string Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Room, RoomDetailVm>();
+        }
     }
 }
