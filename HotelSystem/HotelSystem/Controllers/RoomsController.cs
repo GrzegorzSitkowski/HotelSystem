cdd3d7a57d47d7cc97cdb801f64bcb372ca2f53b
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-15 18:02:42 +0200

    Created RoomsController

---
+++ HotelSystem/HotelSystem/Controllers/RoomsController.cs
@@ -0,0 +1,22 @@
+﻿using HotelSystem.Persistance;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Api.Controllers
+{
+    [Route("api/rooms")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private readonly HotelDbContext _context;
+
+        public RoomsController(HotelDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
