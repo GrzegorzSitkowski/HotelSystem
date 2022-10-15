d85c3a09a410954ac64055aa7f444fa1209d37ce
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-15 17:59:59 +0200

    Implemented GetRoomsQueryHandler

--- HotelSystem/HotelSystem.Application/Rooms/Queries/GetRooms/GetRoomsQuery.cs
+++ HotelSystem/HotelSystem.Application/Rooms/Queries/GetRooms/GetRoomsQuery.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HotelSystem.Application.Rooms.Queries.GetRooms
 {
-    class GetRoomsQuery
+    public class GetRoomsQuery : IRequest<RoomsVm>
     {
     }
 }
---
+++ HotelSystem/HotelSystem.Application/Rooms/Queries/GetRooms/GetRoomsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using HotelSystem.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Rooms.Queries.GetRooms
+{
+    public class GetRoomsQueryHandler : IRequestHandler<GetRoomsQuery, RoomsVm>
+    {
+        private readonly IHotelDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetRoomsQueryHandler(IHotelDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<RoomsVm> Handle(GetRoomsQuery request, CancellationToken none)
+        {
+            var rooms = await _context.Rooms.AsNoTracking().Where(p => p.StatusId == 1 && p.Avability == true).
+                ProjectTo<RoomsVm>(_mapper.ConfigurationProvider).ToListAsync();
+
+            return new RoomsVm();
+        }
+    }
+}
