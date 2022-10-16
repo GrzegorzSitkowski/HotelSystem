8ca5db40bfdc9442e2e0a8f3908ebdb9241328c3
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-16 11:11:41 +0200

    Added GetReservationDetailQuery

---
+++ HotelSystem/HotelSystem.Application/Reservations/Queries/GetReservationDetail/GetReservationDetailQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Reservations.Queries.GetReservationDetail
+{
+    public class GetReservationDetailQuery : IRequest<ReservationDetailVm>
+    {
+        public int Id { get; set; }
+    }
+}
