883dc1f5905e29b74c554aa3b4d21d037e493d1f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-16 12:44:24 +0200

    Added UpdateReservationCommand

---
+++ HotelSystem/HotelSystem.Application/Reservations/Commands/UpdateReservation/UpdateReservationCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Reservations.Commands.UpdateReservation
+{
+    public class UpdateReservationCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Mail { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public bool Payment { get; set; }
+    }
+}
