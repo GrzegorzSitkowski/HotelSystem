5cd5868e8de9e19cf7def3682060597969ea621f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-16 11:45:55 +0200

    Added CreateReservationCommandHandler

---
+++ HotelSystem/HotelSystem.Application/Reservations/Commands/CreateReservation/CreateReservationCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using HotelSystem.Application.Interfaces;
+using HotelSystem.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Reservations.Commands.CreateReservation
+{
+    public class CreateReservationCommandHandler : IRequestHandler<CreateReservationCommand, int>
+    {
+        private readonly IHotelDbContext _context;
+
+        public CreateReservationCommandHandler(IHotelDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Handle(CreateReservationCommand request, CancellationToken cancellationToken)
+        {
+            Reservation reservation = new()
+            {
+                Price = request.Price,
+                Mail = request.Mail,
+                CheckIn = request.CheckIn,
+                CheckOut = request.CheckOut,
+                Payment = request.Payment,
+                UserId = request.UserId,
+                RoomId = request.RoomId
+            };
+
+            _context.Reservations.Add(reservation);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return reservation.Id;
+        }
+    }
+}
