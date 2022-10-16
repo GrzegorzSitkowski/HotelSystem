8b320c4821ff28e997d85308d633afa4799998ac
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-16 12:52:54 +0200

    Added DeleteReservationCommands

---
+++ HotelSystem/HotelSystem.Application/Reservations/Commands/DeleteReservation/DeleteReservationCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using HotelSystem.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Reservations.Commands.DeleteReservation
+{
+    public class DeleteReservationCommandHandler : IRequestHandler<DeleteReservationCommand>
+    {
+        private readonly IHotelDbContext _context;
+        public DeleteReservationCommandHandler(IHotelDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(DeleteReservationCommand request, CancellationToken cancellationToken)
+        {
+            var reservation = await _context.Reservations.Where(p => p.Id == request.Id).FirstOrDefaultAsync(cancellationToken);
+
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
