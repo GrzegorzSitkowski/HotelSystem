1dbe91d612bbc0995c206e5378fc25452076404f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-16 11:34:37 +0200

    Added GetReservationsQueryHandler

---
+++ HotelSystem/HotelSystem.Application/Reservations/Queries/GetReservations/GetReservationsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using HotelSystem.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Reservations.Queries.GetReservations
+{
+    public class GetReservationsQueryHandler : IRequestHandler<GetReservationsQuery, ReservationsVm>
+    {
+        private readonly IHotelDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetReservationsQueryHandler(IHotelDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<ReservationsVm> Handle(GetReservationsQuery request, CancellationToken cancellationToken)
+        {
+            var reservations = await _context.Reservations.AsNoTracking().Where(p => p.StatusId == 1)
+                .ProjectTo<ReservationsVm>(_mapper.ConfigurationProvider).ToListAsync();
+
+            return new ReservationsVm();
+        }
+    }
+}
