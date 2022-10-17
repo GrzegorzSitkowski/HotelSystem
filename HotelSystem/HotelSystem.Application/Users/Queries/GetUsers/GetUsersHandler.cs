c532c00139ff03d40583af0781c8230f49781110
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-17 20:30:31 +0200

    Added GetUsersQuery

---
+++ HotelSystem/HotelSystem.Application/Users/Queries/GetUsers/GetUsersHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Users.Queries.GetUsers
+{
+    public class GetUsersHandler : IRequest<UsersVm>
+    {
+    }
+}
