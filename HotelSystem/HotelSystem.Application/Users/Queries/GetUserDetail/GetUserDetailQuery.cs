df4c40096e442a9e7b840b53eae19cd23c4c034c
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-17 20:24:16 +0200

    Added GetUserDetailQueryHandler

---
+++ HotelSystem/HotelSystem.Application/Users/Queries/GetUserDetail/GetUserDetailQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Users.Queries.GetUserDetail
+{
+    public class GetUserDetailQuery : IRequest<UserDetailVm>
+    {
+        public int Id { get; set; }
+    }
+}
