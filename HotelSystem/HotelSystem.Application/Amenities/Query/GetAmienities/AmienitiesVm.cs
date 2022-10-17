a7d0277082e8d48001481f7ac34b9982919edc29
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-17 20:17:21 +0200

    Added UserDetailVm

---
+++ HotelSystem/HotelSystem.Application/Amenities/Query/GetAmienities/AmienitiesVm.cs
@@ -0,0 +1,14 @@
+﻿using HotelSystem.Application.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Amenities.Query.GetAmienities
+{
+    public class AmienitiesVm : IMapFrom<Domain.Entities.Amenities>
+    {
+
+    }
+}
