c3b78f1b0f9f57a21d881def31c85f0697911b48
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-17 20:29:27 +0200

    Added UsersVm

---
+++ HotelSystem/HotelSystem.Application/Users/Queries/GetUsers/UsersVm.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HotelSystem.Application.Mappings;
+using HotelSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Users.Queries.GetUsers
+{
+    public class UsersVm : IMapFrom<User>
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Mail { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<User, UsersVm>();
+        }
+    }
+}
