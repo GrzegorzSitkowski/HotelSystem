97fa1f99e7cf76d9985c3c49a1dc4877e40d617f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-10-20 19:42:19 +0200

    Created ReservationsController

---
+++ HotelSystem/HotelSystem/Controllers/ReservationsController.cs
@@ -0,0 +1,21 @@
+﻿using HotelSystem.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Api.Controllers
+{
+    [Route("api/reservations")]
+    public class ReservationsController : BaseController
+    {
+        private readonly HotelDbContext _context;
+
+        public ReservationsController(HotelDbContext context)
+        {
+            _context = context;
+        }
+        
+    }
+}
