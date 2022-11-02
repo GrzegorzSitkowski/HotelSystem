fd1282c416493bbf9209c382b6342e82feb16cb7
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-11-02 18:52:34 +0100

    Added - CreateRoomCommandValidator

---
+++ HotelSystem/HotelSystem.Application/Rooms/Commands/CreateRoom/CreateRoomCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Rooms.Commands.CreateRoom
+{
+    public class CreateRoomCommandValidator : AbstractValidator<CreateRoomCommand>
+    {
+        public CreateRoomCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.Capacity).NotEmpty();
+            RuleFor(x => x.Price).NotEmpty();
+            RuleFor(x => x.Description).MaximumLength(100);
+        }
+    }
+}
