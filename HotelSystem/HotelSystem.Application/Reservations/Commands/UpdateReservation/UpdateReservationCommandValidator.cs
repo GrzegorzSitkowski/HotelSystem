3374364e798d83051be7ee27ea2ae8142a3be42a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-11-02 18:49:31 +0100

    Added - UpdateReservationCommandValidator

---
+++ HotelSystem/HotelSystem.Application/Reservations/Commands/UpdateReservation/UpdateReservationCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Reservations.Commands.UpdateReservation
+{
+    public class UpdateReservationCommandValidator : AbstractValidator<UpdateReservationCommand>
+    {
+        public UpdateReservationCommandValidator()
+        {
+            RuleFor(x => x.Price).NotEmpty();
+            RuleFor(x => x.Mail).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.CheckIn).NotEmpty();
+            RuleFor(x => x.CheckOut).NotEmpty();
+        }
+    }
+}
