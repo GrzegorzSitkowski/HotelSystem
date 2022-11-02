fca6f92b26196b33de501badbb0078603efbbbbc
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-11-02 18:53:32 +0100

    Added - UpdateRoomCommandValidator

---
+++ HotelSystem/HotelSystem.Application/Rooms/Commands/UpdateRoom/UpdateRoomCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Rooms.Commands.UpdateRoom
+{
+    public class UpdateRoomCommandValidator : AbstractValidator<UpdateRoomCommand>
+    {
+        public UpdateRoomCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.Capacity).NotEmpty();
+            RuleFor(x => x.Price).NotEmpty();
+            RuleFor(x => x.Description).MaximumLength(100);
+        }
+    }
+}
