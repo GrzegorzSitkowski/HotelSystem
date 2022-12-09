bc65d3a50aa264d1f752cbcb7a8373c9229dbc9d
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-12-09 20:29:43 +0100

    Added CurrentUserService.cs in HotelDbContext.cs

--- HotelSystem/HotelSystem.Persistance/HotelDbContext.cs
+++ HotelSystem/HotelSystem.Persistance/HotelDbContext.cs
@@ -15,15 +15,17 @@ namespace HotelSystem.Persistance
     public class HotelDbContext : DbContext, IHotelDbContext
     {
         private readonly IDateTime _dateTime;
+        private readonly ICurrentUserService _userService;
 
         public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options)
         {
 
         }
 
-        public HotelDbContext(DbContextOptions<HotelDbContext> options, IDateTime dateTime) : base(options)
+        public HotelDbContext(DbContextOptions<HotelDbContext> options, IDateTime dateTime, ICurrentUserService userService) : base(options)
         {
             _dateTime = dateTime;
+            _userService = userService;
         }
      
         public DbSet<Amenities> Amenities { get; set; }
@@ -44,19 +46,19 @@ namespace HotelSystem.Persistance
                 switch(entry.State)
                 {
                     case EntityState.Added:
-                        entry.Entity.CreatedBy = string.Empty;
+                        entry.Entity.CreatedBy = _userService.Email;
                         entry.Entity.Created = DateTime.Now;
                         entry.Entity.StatusId = 1;
                         break;
                     case EntityState.Modified:
-                        entry.Entity.ModifiedBy = string.Empty;
+                        entry.Entity.ModifiedBy = _userService.Email;
                         entry.Entity.Modified = DateTime.Now;
                         break;
                     case EntityState.Deleted:
-                        entry.Entity.ModifiedBy = string.Empty;
+                        entry.Entity.ModifiedBy = _userService.Email;
                         entry.Entity.Modified = DateTime.Now;
                         entry.Entity.Inactivated = DateTime.Now;
-                        entry.Entity.InactivatedBy = string.Empty;
+                        entry.Entity.InactivatedBy = _userService.Email;
                         entry.Entity.StatusId = 0;
                         entry.State = EntityState.Modified;
                         break;
