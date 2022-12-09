6ed2326a8dbe31c58b6eb9e8b659cdebf25a50d0
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-12-09 20:21:44 +0100

    Created CurrentUserService

---
+++ HotelSystem/HotelSystem/Service/CurrentUserService.cs
@@ -0,0 +1,25 @@
+﻿using HotelSystem.Application.Interfaces;
+using IdentityModel;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Api.Service
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public string Email { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            var email = httpContextAccessor.HttpContext?.User?.FindFirstValue(JwtClaimTypes.Email);
+
+            Email = Email;
+
+            IsAuthenticated = !string.IsNullOrEmpty(email);
+        }
+    }
+}
