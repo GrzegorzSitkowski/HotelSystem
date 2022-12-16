578001eea3abf1d0650cab26a6babdda0b007e86
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-12-16 19:46:26 +0100

    Implemented MappingTest.cs

--- HotelSystem/Application.UnitTests/Mapping/MappingTest.cs
+++ HotelSystem/Application.UnitTests/Mapping/MappingTest.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Application.UnitTests.Mapping
 {
-    public class MappingTest
+    public class MappingTest : IClassFixture<MappingTestFixture>
     {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTest(MappingTestFixture fixture)
+        {
+            _configuration = fixture.ConfigurationProvider;
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
     }
 }
