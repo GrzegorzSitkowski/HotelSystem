8e47954c9d43aa2b2e7293d38be41671e85c4dd9
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-12-20 21:04:18 +0100

    Added GetReservationDetailQueryHandlerTests.cs

---
+++ HotelSystem/Application.UnitTests/Reservation/Queries/GetReservationDetailQueryHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using Application.UnitTests.Common;
+using AutoMapper;
+using HotelSystem.Application.Reservations.Queries.GetReservationDetail;
+using HotelSystem.Application.Reservations.Queries.GetReservations;
+using HotelSystem.Persistance;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Reservation.Queries
+{
+    [Collection("QueryCollection")]
+    public class GetReservationDetailQueryHandlerTests
+    {
+        private readonly HotelDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetReservationDetailQueryHandlerTests(QueryTestFixtures fixture)
+        {
+            _context = fixture.Context;
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public async Task CanGetReservationDetalById()
+        {
+            var handler = new GetReservationDetailQueryHandler(_context, _mapper);
+            var reservationId = 2;
+
+            var result = await handler.Handle(new GetReservationDetailQuery { Id = reservationId }, CancellationToken.None);
+
+            result.ShouldBeOfType<ReservationDetailVm>();
+            result.Price.ShouldBe(300);
+        }
+    }
+}
