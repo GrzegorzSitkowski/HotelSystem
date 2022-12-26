5dfa602322c86c9b192533bcc17215e32efc1f75
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-12-26 14:26:37 +0100

    Added GetRoomsQueryHandlerTests.cs

---
+++ HotelSystem/Application.UnitTests/Room/Queries/GetRooms/GetRoomsQueryHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using Application.UnitTests.Common;
+using AutoMapper;
+using HotelSystem.Application.Rooms.Queries.GetRooms;
+using HotelSystem.Persistance;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Room.Queries.GetRooms
+{
+    [Collection("QueryCollection")]
+    public class GetRoomsQueryHandlerTests
+    {
+        private readonly HotelDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetRoomsQueryHandlerTests(QueryTestFixtures fixtures)
+        {
+            _context = fixtures.Context;
+            _mapper = fixtures.Mapper;
+        }
+
+        [Fact]
+        public async Task CanGetRooms()
+        {
+            var handler = new GetRoomsQueryHandler(_context, _mapper);
+
+            var result = await handler.Handle(new GetRoomsQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<RoomsVm>();
+        }
+    }
+}
