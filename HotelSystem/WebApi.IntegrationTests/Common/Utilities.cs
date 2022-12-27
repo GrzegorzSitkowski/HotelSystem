b8627ecdc2b8ffba744be5f7c5625390fb0d5ce9
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-12-27 17:14:38 +0100

    Created Utilities.cs

---
+++ HotelSystem/WebApi.IntegrationTests/Common/Utilities.cs
@@ -0,0 +1,40 @@
+﻿using HotelSystem.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.IntegrationTests.Common
+{
+    public class Utilities
+    {
+        public static void InitializeDbForTests(HotelDbContext context)
+        {
+            var reservation = new HotelSystem.Domain.Entities.Reservation()
+            {
+                Id = 2,
+                Price = 300,
+                Mail = "mail@example.com",
+                Payment = true,
+                UserId = 1,
+                RoomId = 15,
+                StatusId = 1
+            };
+            context.Reservations.Add(reservation);
+
+            var room = new HotelSystem.Domain.Entities.Room()
+            {
+                Id = 15,
+                Name = "Double room",
+                Capacity = 2,
+                Price = 260,
+                Avability = true,
+                Description = "Room for two.",
+                StatusId = 1
+            };
+            context.Rooms.Add(room);
+
+            context.SaveChanges();
+        }
+    }
+}
