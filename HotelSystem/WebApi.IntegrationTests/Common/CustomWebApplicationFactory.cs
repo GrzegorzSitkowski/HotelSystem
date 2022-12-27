9b62276bb97cb2cd7a742551cc87916ba4d54804
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-12-27 14:23:14 +0100

    Installed - Microsoft.AspNetCore.Mvc.Testing

---
+++ HotelSystem/WebApi.IntegrationTests/Common/CustomWebApplicationFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.IntegrationTests.Common
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+    }
+}
