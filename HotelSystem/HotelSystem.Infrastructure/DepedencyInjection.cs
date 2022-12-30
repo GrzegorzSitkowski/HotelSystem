c897afd562800c4e4ff9820b658d3f4e833622c2
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-12-30 18:02:29 +0100

    Finished configuration Sturtup.cs

--- HotelSystem/HotelSystem.Infrastructure/DepedencyInjection.cs
+++ HotelSystem/HotelSystem.Infrastructure/DepedencyInjection.cs
@@ -3,11 +3,7 @@ using HotelSystem.Infrastructure.FileStore;
 using HotelSystem.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HotelSystem.Infrastructure
 {
