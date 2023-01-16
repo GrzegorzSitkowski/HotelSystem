82986f4330e791562460218440918a7c3bcf0aa1
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-01-16 19:57:36 +0100

    Created migration - PhotoOfRoom

--- HotelSystem/HotelSystem.Persistance/Migrations/20230116185655_PhotoOfRoom.Designer.cs
+++ HotelSystem/HotelSystem.Persistance/Migrations/20230116185655_PhotoOfRoom.Designer.cs
@@ -4,14 +4,16 @@ using HotelSystem.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelSystem.Persistance.Migrations
 {
     [DbContext(typeof(HotelDbContext))]
-    partial class HotelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230116185655_PhotoOfRoom")]
+    partial class PhotoOfRoom
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
