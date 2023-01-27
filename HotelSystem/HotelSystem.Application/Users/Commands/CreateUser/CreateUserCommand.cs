d27c458df367a9053668bed16ab70089ed662227
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-01-27 19:31:34 +0100

    CreateUserCommand has been corrected

--- HotelSystem/HotelSystem.Application/Users/Commands/CreateUser/CreateUserCommand.cs
+++ HotelSystem/HotelSystem.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -9,10 +9,14 @@ namespace HotelSystem.Application.Users.Commands.CreateUser
 {
     public class CreateUserCommand : IRequest<int>
     {
-        public string Type { get; set; }
-        public string Mail { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Type { get; set; }
+        public string Mail { get; set; }
         public string Password { get; set; }
+        public string PhoneNumner { get; set; }
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
     }
 }
