74333f12e7e6b8a426562b78d446865f16669f3c
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-01-27 18:54:00 +0100

    Modified entity of user

--- HotelSystem/HotelSystem.Domain/Entities/User.cs
+++ HotelSystem/HotelSystem.Domain/Entities/User.cs
@@ -10,10 +10,16 @@ namespace HotelSystem.Domain.Entities
 {
     public class User : AuditableEntity
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Type { get; set; }
         public string Mail { get; set; }
-        public PersonName UserName {get; set;}
         public string Password { get; set; }
+        public string PhoneNumner { get; set; }
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+
         public ICollection<Reservation> Reservations { get; set; }
     }
 }
