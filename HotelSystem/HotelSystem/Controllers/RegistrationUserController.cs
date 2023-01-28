67ea8b22ec4087cfa101e255086714debc55f7b6
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-01-28 19:50:26 +0100

    Added HttpGet - GetUserDetail

--- HotelSystem/HotelSystem/Controllers/RegistrationUserController.cs
+++ HotelSystem/HotelSystem/Controllers/RegistrationUserController.cs
@@ -1,4 +1,5 @@
 ﻿using HotelSystem.Application.Users.Commands.CreateUser;
+using HotelSystem.Application.Users.Queries.GetUserDetail;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,12 +11,19 @@ namespace HotelSystem.Api.Controllers
     public class RegistrationUserController : BaseController
     {
         [Route("api/registration")]
-        
+
         [HttpPost]
         public async Task<IActionResult> RegisterUser(CreateUserCommand command)
         {
             var result = await Mediator.Send(command);
             return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDetailVm>> GetUserDetail(int id)
+        {
+            var vm = await Mediator.Send(new GetUserDetailQuery() { Id = id });
+            return vm;
         }
     }
 }
