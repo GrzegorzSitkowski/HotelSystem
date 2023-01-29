77545dea95efc2b40884b7ee70606c8849af5245
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-01-29 12:29:07 +0100

    Implemented registration.model.ts

---
+++ ClientHotelSystem/src/app/models/users/registration.model.ts
@@ -0,0 +1,12 @@
+export interface User{
+    id: string;
+    firstName: string;
+    lastName: string;
+    type: string;
+    mail: string;
+    password: string;
+    phoneNumber: string;
+    addres: string;
+    postCode: string;
+    city: string;
+}
