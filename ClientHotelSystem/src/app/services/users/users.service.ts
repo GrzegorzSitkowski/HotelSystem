bec62eb5c50c6b897890856a8e78954c44881b24
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-01-29 12:46:50 +0100

    Implemented HttpPost in userService

--- ClientHotelSystem/src/app/services/users/users.service.ts
+++ ClientHotelSystem/src/app/services/users/users.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/users/registration.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  baseApiUrl: string = environment.baseApiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  addUser(addUserRequest: User): Observable<User>{
+    return this.http.post<User>(this.baseApiUrl + '/api/users', addUserRequest);
+  }
 }
