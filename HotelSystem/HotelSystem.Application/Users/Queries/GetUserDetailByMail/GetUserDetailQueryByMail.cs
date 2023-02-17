971d0bed1c0da1a2e6a974e16ac3f04d1771657e
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-17 16:19:47 +0100

    Added GetUserDetailQueryByMail

---
+++ HotelSystem/HotelSystem.Application/Users/Queries/GetUserDetailByMail/GetUserDetailQueryByMail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Users.Queries.GetUserDetailByMail
+{
+    public class GetUserDetailQueryByMail
+    {
+        public string Mail { get; set; }
+    }
+}
