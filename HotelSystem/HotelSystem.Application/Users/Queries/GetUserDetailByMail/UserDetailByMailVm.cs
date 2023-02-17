3e5ee7151e9c5129e4e5ea45c16925d57b0ffbf9
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-17 16:21:22 +0100

    Added UserDetailByMailVm

---
+++ HotelSystem/HotelSystem.Application/Users/Queries/GetUserDetailByMail/UserDetailByMailVm.cs
@@ -0,0 +1,32 @@
+﻿]using AutoMapper;
+using HotelSystem.Application.Mappings;
+using HotelSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Application.Users.Queries.GetUserDetailByMail
+{
+    public class UserDetailByMailVm : IMapFrom<User>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Type { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumner { get; set; }
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<User, UserDetailByMailVm>();
+        }
+    }
+}
