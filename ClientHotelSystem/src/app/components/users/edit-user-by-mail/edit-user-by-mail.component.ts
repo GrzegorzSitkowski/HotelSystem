298a99fc4f7323ef660f9f3590430328b84c8e3e
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-19 18:03:56 +0100

    Implemented edit-user-by-mail.components.ts

--- ClientHotelSystem/src/app/components/users/edit-user-by-mail/edit-user-by-mail.component.ts
+++ ClientHotelSystem/src/app/components/users/edit-user-by-mail/edit-user-by-mail.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/models/users/registration.model';
+import { UsersService } from 'src/app/services/users/users.service';
 
 @Component({
-  selector: 'app-edit-user-by-mail',
+  selector: 'app-edit-user',
   templateUrl: './edit-user-by-mail.component.html',
   styleUrls: ['./edit-user-by-mail.component.css']
 })
 export class EditUserByMailComponent implements OnInit {
 
-  constructor() { }
+  userDetails: User = {
+    id: '0',
+    firstName: '',
+    lastName: '',
+    type: '',
+    mail: '',
+    password: '',
+    phoneNumner: '',
+    address: '',
+    postCode: '',
+    city: ''
+  };
+  
+  constructor(private route: ActivatedRoute, private userService: UsersService, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const mail = params.get('mail');
+
+        if(mail){
+          this.userService.getUser(mail)
+          .subscribe({
+            next: (response) => {
+              this.userDetails = response;
+            }
+          });
+        }
+      }
+    })
+  }
+
+  updateUser(){
+    this.userService.updateUser(this.userDetails.mail,
+      this.userDetails)
+      .subscribe({
+        next: (response) => {
+          this.router.navigate(['users']);
+        }
+      })
+  }
+
+  deleteUser(mail: string){
+    this.userService.deleteUser(mail)
+    .subscribe({
+      next: (response) => {
+        this.router.navigate(['users']);
+      }
+    })
   }
 
 }
