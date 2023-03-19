c6662ea47b4f8356411d2d179df41b18da71c221
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-03-19 18:01:38 +0100

    Created entity - JwtService

--- HotelSystem/HotelSystem/Controllers/UsersController.cs
+++ HotelSystem/HotelSystem/Controllers/UsersController.cs
@@ -71,14 +71,11 @@ namespace HotelSystem.Api.Controllers
         [HttpPost("LoginUser")]
         public IActionResult Login(LoginUser user)
         {
-            //user.Authorization = false;
             var userAvailable = _context.Users.Where(u => u.Mail == user.Email && u.Password == user.Password).FirstOrDefault();
             if(userAvailable != null)
             {
-                //user.Authorization = true;
                 return Ok("Success");
             }
-
             return Ok("Failure");
         }
     }
