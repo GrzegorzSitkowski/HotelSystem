46681307d5ff1de54ae7c1f78b65541d2028fc5a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-03-19 20:38:55 +0100

    Implemented JwtService

--- HotelSystem/HotelSystem.Domain/Entities/JwtService.cs
+++ HotelSystem/HotelSystem.Domain/Entities/JwtService.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +12,42 @@ namespace HotelSystem.Domain.Entities
 {
     public class JwtService
     {
+        public String SecretKey { get; set; }
+        public int TokenDuration { get; set; }
+
+        private readonly IConfiguration config;
+
+        public JwtService(IConfiguration _config)
+        {
+            config = _config;
+            this.SecretKey = config.GetSection("jwtConfig").GetSection("Key").Value;
+            this.TokenDuration = Int32.Parse(config.GetSection("jwtConfig").GetSection("Duration").Value);
+        }
+
+        public string GenerateToken(string id, string firstName, string lastName, string email, string phoneNumber)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.SecretKey));
+
+            var signature = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var payload = new[]
+            {
+                new Claim("id", id),
+                new Claim("firstName", firstName),
+                new Claim("lastName", lastName),
+                new Claim("email", email),
+                new Claim("phoneNumber", phoneNumber)
+            };
+
+            var jwtToken = new JwtSecurityToken(
+                issuer: "localhost",
+                audience: "localhost",
+                claims: payload,
+                expires: DateTime.Now.AddMinutes(TokenDuration),
+                signingCredentials: signature
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+        }
     }
 }
